e04f61aec8b9873dcaf34826afb2e57250931b9a
M. Washington <mickaell.washington@hotmail.com>
2023-04-11 10:42:26 -0300

    Create Aula2part2.java

---
+++ Aula2part2.java
@@ -0,0 +1,40 @@
+package aula2part2;
+public class Aula2part2 {
+    public static void main(String[] args) {
+        String name1 = "Camaleão";
+        int year1 = 18;
+        double height1 = 1.45;
+        
+        String name2 = "3Horas";
+        int year2 = 25;
+        double height2 = 1.68;
+        
+        String name3 = "UnhaPreta";
+        int year3 = 32;
+        double height3 = 1.79;
+        
+        String name4 = "Soneca";
+        int year4 = 56;
+        double height4 = 1.54;
+        
+        System.out.println("Welcome Mr "+name1);
+        System.out.println("You are "+year1+ " years old.");
+        System.out.println("And have "+height1+ " height");
+        System.out.println(" ");
+        
+        System.out.println("Welcome Mr "+name2);
+        System.out.println("You are "+year2+ " years old.");
+        System.out.println("And have "+height2+ " height");
+        System.out.println(" ");
+        
+        System.out.println("Welcome Mr "+name3);
+        System.out.println("You are "+year3+ " years old.");
+        System.out.println("And have "+height3+ " height");
+        System.out.println(" ");
+        
+        System.out.println("Welcome Mr "+name4);
+        System.out.println("You are "+year4+ " years old.");
+        System.out.println("And have "+height4+ " height");
+    }
+    
+}
