8c196a30a24d5572602a9f95b8f6d9c36bc6dada
M. Washington <mickaell.washington@hotmail.com>
2023-04-11 11:38:38 -0300

    Create Aula2part3.java

---
+++ Aula2part3.java
@@ -0,0 +1,17 @@
+package aula2part3;
+
+import java.util.Scanner;
+
+public class Aula2part3 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in); 
+        
+            String name;
+            
+            System.out.println("Your name: ");
+            name = read.nextLine();
+             
+            System.out.println("Your name is: "+name);
+    }
+    
+}
