096f09acfe676447c72e7fcd67d11ed55eabee7e
M. Washington <mickaell.washington@hotmail.com>
2023-04-11 10:05:41 -0300

    Create Aula211042023.java

---
+++ Aula211042023.java
@@ -0,0 +1,13 @@
+package aula2.pkg11042023;
+public class Aula211042023 {
+    public static void main(String[] args) {
+        String name = "Washington";
+        int years = 20;
+        double height = 1.80;
+        
+        System.out.println("Welcome Mr "+name);
+        System.out.println("You are "+years+ " years old.");
+        System.out.println("And have "+height+ " height");
+    }
+    
+}
