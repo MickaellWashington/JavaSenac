5b81be8ceb5869a79c966112661d833e59cdfe40
M. Washington <mickaell.washington@hotmail.com>
2023-04-13 11:56:49 -0300

    aula4

---
+++ Aula4part2exercicoAritmetico.java
@@ -0,0 +1,12 @@
+package aula4part2exercicoaritmetico;
+
+import java.util.Scanner;
+
+public class Aula4part2exercicoAritmetico {
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
---
+++ JavaApplication26/src/javaapplication26/JavaApplication26.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication26;
+
+/**
+ *
+ * @author alunosenac
+ */
+public class JavaApplication26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double n1 = 10000;
+        double n2 = 10000;
+        double resultado1;
+        double resultado2;
+
+        resultado1 = (n1 * 45)/100;
+        resultado2 = (n2 * 10)/45;
+                
+        System.out.println(n1);
+        System.out.println(n2);
+    }
+    
+}
