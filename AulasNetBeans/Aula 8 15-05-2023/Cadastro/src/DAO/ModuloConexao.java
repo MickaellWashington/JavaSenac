1417f49846644e66d620115fd59b5616e092799c
Washington <mickaell.washington@hotmail.com>
2023-05-15 11:41:57 -0300

    Aula 8 15-05-2023

---
+++ AulasNetBeans/Aula 8 15-05-2023/Cadastro/src/DAO/ModuloConexao.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.*;
+
+/**
+ *
+ * @author alunosenac
+ */
+public class ModuloConexao {
+    
+    public static Connection conectar(){
+        Connection conexao;
+        
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://locatehost:3306/Cadastro";
+        String user = "root";
+        String password = "";
+        
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+            
+            } catch(ClassNotFoundException | SQLException e){
+                System.out.println(e);
+                return null;
+            }
+        
+    }
+    
+}
