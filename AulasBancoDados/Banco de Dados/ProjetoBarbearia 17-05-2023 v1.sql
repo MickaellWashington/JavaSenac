4d0fea96c91cadffb80727cdf6717f55d7f142f1
Washington <mickaell.washington@hotmail.com>
2023-05-17 11:48:11 -0300

    Create ProjetoBarbearia 17-05-2023 v1.sql

---
+++ AulasBancoDados/Banco de Dados/ProjetoBarbearia 17-05-2023 v1.sql
@@ -0,0 +1,44 @@
+create database barbearia;
+use barbearia;
+
+create table pessoa(
+id_pessoa int not null auto_increment primary key,
+nome varchar(45) not null,
+endereco varchar(45) not null,
+telefone varchar(20) not null,
+email varchar(45) not null
+);
+
+create table cliente(
+id_cliente int not null auto_increment primary key,
+pessoa_id int not null,
+foreign key (pessoa_id) references pessoa(id_pessoa)
+);
+
+create table administrador(
+id_administrador int not null auto_increment primary key,
+login varchar(45) not null,
+senha varchar(45) not null,
+pessoa_id int not null,
+foreign key (pessoa_id) references pessoa(id_pessoa)
+);
+
+create table funcionario(
+id_funcionario int not null auto_increment primary key,
+cargo varchar(45) not null,
+senha varchar(45) not null,
+pessoa_id int not null,
+foreign key (pessoa_id) references pessoa(id_pessoa)
+);
+
+create table servico(
+id_servico int not null auto_increment primary key,
+descricao varchar(45),
+dt_inicio date,
+hr_inicio datetime,
+valor decimal,
+funcionario_id int not null,
+foreign key (funcionario_id) references funcionario(id_funcionario),
+cliente_id int not null,
+foreign key (cliente_id) references cliente(id_cliente)
+);
